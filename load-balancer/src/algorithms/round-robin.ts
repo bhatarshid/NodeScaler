13ef7b0497ce88a524e82b882252a7cba2d55507
arshid.bhat <bhatarshid1000@gmail.com>
2024-12-28 00:30:18 +0530

    added proxy

--- load-balancer/src/algorithms/round-robin.ts
+++ load-balancer/src/algorithms/round-robin.ts
@@ -1,28 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { PORTS } from "../index.js";
-import http from 'http';
+import httpProxy from 'http-proxy';
 
 let counter = 0;
+const proxy = httpProxy.createProxyServer();
+
 export default function roundRobinAlgo(req: Request, res: Response, next: NextFunction) {
   const serverIndex = counter % PORTS.length;
   counter++;
   const port = PORTS[serverIndex];
-  const proxy = http.request({
-    host: 'localhost',
-    port: port,
-    path: req.url,
-    method: req.method,
-    headers: req.headers
-  }, (response: any) => {
-    console.log(`Proxying request to port ${port}`);
-    res.writeHead(response.statusCode, response.headers);
-    response.pipe(res, { end: true });
-  });
 
-  proxy.on('error', (err) => {
-    console.error(`Error proxying request to port ${port}:`, err);
-    res.status(500).send('Internal Server Error');
+  proxy.web(req, res, { target: `http://localhost:${port}` }, (err) => {
+    if (err) {
+      console.log(`Error proxy: ${err}`); 
+      res.status(500).send('Internal Server Error');
+    }
   });
-
-  req.pipe(proxy, { end: true });
 }
